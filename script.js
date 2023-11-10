a9860621ef1eb6c92d071b6dcf96f3f82d1d28e6
DragonflyValkyrie <148598429+DragonflyValkryrie@users.noreply.github.com>
2023-11-09 19:00:11 -0500

    Enhance Drawing Functionality and Add Styling
    Introduce the ability to draw on the grid using the added
    functions `rainbowColor` and `defaultColor`. The `rainbowColor`
    function generates a random color for a vibrant drawing experience.
    The `defaultColor` function sets the default drawing color to grey.
    
    Additionally, implement the drawing functionality by changing the
    square's background color on mouse enter using the `defaultColor`
    function. Header and footer sections are also added for improved
    visual structure.
    
    While the buttons currently lack functionality, users can now enjoy
    drawing in the grid.

--- script.js
+++ script.js
@@ -65,3 +65,18 @@ function createGrid(size) {
 };
 
 createGrid(gridSize);
+
+
+function rainbowColor() {
+    const letters ="0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++){
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+};
+
+function defaultColor(){
+    color = "grey";
+    return color;
+};
